e636e0cd249bfa143e828b8cfc7d7362a5e5ea4d
AliveRg <renatgorodov49280@gmail.com>
2023-05-30 14:38:04 +0300

    test

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,12 +2,12 @@
 import { Head, Link } from "@inertiajs/vue3";
 
 defineProps({
-    canLogin: {
-        type: Boolean,
-    },
-    canRegister: {
-        type: Boolean,
-    },
+    // false: {
+    //     type: Boolean,
+    // },
+    // true: {
+    //     type: Boolean,
+    // },
     users: {
         type: Object,
     },
@@ -20,12 +20,9 @@ defineProps({
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
     >
-        <div
-            v-if="canLogin"
-            class="sm:fixed sm:top-0 sm:right-0 p-6 text-right"
-        >
+        <div v-if="false" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
             <Link
-                v-if="$page.props.auth.user"
+                v-if="false"
                 :href="route('dashboard')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                 >Dashboard</Link
@@ -39,19 +36,16 @@ defineProps({
                 >
 
                 <Link
-                    v-if="canRegister"
+                    v-if="true"
                     :href="route('register')"
                     class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                     >Register</Link
                 >
             </template>
         </div>
-        <div
-            v-if="canLogin"
-            class="sm:fixed sm:top-0 sm:right-0 p-6 text-right"
-        >
+        <div v-if="false" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
             <Link
-                v-if="$page.props.auth.user"
+                v-if="false"
                 :href="route('dashboard')"
                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                 >Dashboard</Link
